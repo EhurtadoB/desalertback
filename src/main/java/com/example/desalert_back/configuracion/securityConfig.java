7ba7d0d5754683da60eecc9dea9dd25c9f0d07ed
Investigacion-02 <investigacion-02@iMac-de-Investigacion-02.local>
2022-05-24 18:53:37 -0500

    Agregando seguridad

--- src/main/java/com/example/desalert_back/configuracion/securityConfig.java
+++ src/main/java/com/example/desalert_back/configuracion/securityConfig.java
@@ -4,17 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 import com.example.desalert_back.seguridad.CustomUserDetailsService;
 
@@ -33,13 +30,25 @@ public class securityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable().authorizeRequests().antMatchers(HttpMethod.GET, "/api/**").permitAll().anyRequest()
-				.authenticated().and().httpBasic();
+		http.csrf().disable()
+			.authorizeRequests().antMatchers(HttpMethod.GET, "/api/**").permitAll()
+			.antMatchers("/api/auth/**").permitAll()
+			.anyRequest()
+			.authenticated()
+			.and()
+			.httpBasic();
 
 	}
 	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+	}
+	
+	@Override
+	@Bean
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		// TODO Auto-generated method stub
+		return super.authenticationManagerBean();
 	}
 }
---
+++ src/main/java/com/example/desalert_back/controllers/AuthController.java
@@ -0,0 +1,31 @@
+package com.example.desalert_back.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.desalert_back.dto.LoginDTO;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+	
+	@Autowired
+	private AuthenticationManager authenticationManager;
+	
+	@PostMapping("/iniciarSesion")
+	public ResponseEntity<String> authenticatedUser(@RequestBody LoginDTO loginDTO){
+		Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginDTO.getUsernameOrEmail(), loginDTO.getPassword()));
+		
+		SecurityContextHolder.getContext().setAuthentication(authentication);	
+		return new ResponseEntity<>("Ha iniciado sesion con exito  !", HttpStatus.OK);
+	}
+}
